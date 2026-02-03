ace3ec8f639a574f25854cdf971dc4a02ac94f0c
Hussein Rashid <husseinrashiduni@gmail.com>
2026-02-03 20:34:53 +0200

    Add restaurant model and frontend pages for list and profile

---
+++ frontend/src/pages/Restaurants.jsx
@@ -0,0 +1,10 @@
+export default function Restaurants() {
+  return (
+    <div>
+      <h1>Restaurants</h1>
+         {/* TODO: Fetch the restaurants from backend */}
+      {/* TODO: Get the RestaurantCard components to here */}
+      <p>Restaurant list will appear here.</p>
+    </div>
+  );
+}
