2ede7ee3759d3db4acfe9f498490576d212bca16
Hussein Rashid <husseinrashiduni@gmail.com>
2026-02-04 10:13:25 +0200

    Add RestaurantCard component and render sample list

--- frontend/src/pages/Restaurants.jsx
+++ frontend/src/pages/Restaurants.jsx
@@ -1,10 +1,20 @@
+import RestaurantCard from "../components/RestaurantCard";
+
 export default function Restaurants() {
+  // Placeholder data for UI testing (remove later when API is ready)
+  const mockRestaurants = [
+    { id: 1, name: "Mailos", cuisine: "Italian", address: "Hamra", rating: 4.5 },
+    { id: 2, name: "Kampai", cuisine: "Japanese", address: "Achrafieh", rating: 4.3 },
+  ];
+
   return (
     <div>
       <h1>Restaurants</h1>
-         {/* TODO: Fetch the restaurants from backend */}
-      {/* TODO: Get the RestaurantCard components to here */}
-      <p>Restaurant list will appear here.</p>
+
+      {/* TODO: Replace mockRestaurants with API response */}
+      {mockRestaurants.map((r) => (
+        <RestaurantCard key={r.id} restaurant={r} />
+      ))}
     </div>
   );
 }
